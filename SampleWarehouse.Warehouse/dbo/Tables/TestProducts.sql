894e8c9c89a336b51a570a96d80b0071a4b5b81d
KevinPenquin <Kevin@fusionsol.com>
2025-07-15 17:27:09 +0700

    Committing 1 item from workspace 7ecf3c24-c79d-4652-b943-565a2c3e7a0b

---
+++ SampleWarehouse.Warehouse/dbo/Tables/TestProducts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[TestProducts] (
+
+	[ProductID] int NOT NULL, 
+	[ProductName] varchar(100) NULL, 
+	[Category] varchar(50) NULL, 
+	[Price] decimal(10,2) NULL, 
+	[InStock] bit NULL
+);
